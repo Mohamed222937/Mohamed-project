848bed2580ade18216dd4a3b8115e2a107cec907
medo2 <medo2@MEDO>
2016-09-15 14:48:09 +0200

    Add project files.

---
+++ Mohamed project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mohamed_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hallo world");
+            Console.ReadKey();
+        }
+    }
+}
